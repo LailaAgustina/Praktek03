a37b8df4b08d03685ebee66c4f1d1a5e17c25157
Laila_Agustina <lailaagustina025@gmail.com>
2018-04-05 02:54:57 -0700

    menggunakan method set

--- src/praktek03/TasAksi.java
+++ src/praktek03/TasAksi.java
@@ -2,11 +2,12 @@ package praktek03;
 public class TasAksi {
     public static void main(String[] args) {
         Tas s = new Tas();
-        s.Merk="ZaraChain";
-        s.Warna="Hitam";
-        s.Bahan="Rajut";
-        s.Model="Selempang";
-        s.harga=150000;
+        
+        s.setMerk("ZaraStore");
+        s.setWarna("Biru");
+        s.setBahan("Kulit");
+        s.setModel("Selempang");
+        s.setHarga(200000);
         
         s.cetakInfo();
     }
