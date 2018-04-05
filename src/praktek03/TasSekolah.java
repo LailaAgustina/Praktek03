ccaf3297b12a1028d54ec684ac1e756d483eee7d
Laila_Agustina <lailaagustina025@gmail.com>
2018-04-05 03:13:22 -0700

    attribute dan method

--- src/praktek03/TasSekolah.java
+++ src/praktek03/TasSekolah.java
@@ -1,5 +1,9 @@
 package praktek03;
 
 public class TasSekolah extends Tas{
-    
+    private String JenisTasSekolah;
+
+    void throttle(){
+        System.out.println("Buku,Pensil,Pulpen,Penggaris,penghapus");
+    }
 }
